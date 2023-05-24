55aa3aa2ecdc596226c25239485d25dc3555d5ff
Naveen <adith.naveen@gmail.com>
2023-05-24 15:41:54 +0530

    producer program

---
+++ workspace/kafka-works/src/main/java/com/ibm/kafka/ProducerExample01.java
@@ -0,0 +1,55 @@
+package com.ibm.kafka;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ProducerExample01 {
+	
+	private static final Logger logger = LoggerFactory.getLogger(ProducerExample01.class.getName());
+	
+	public static void main(String[] args) {
+		logger.info("Hi I'm from slf4j");
+		
+		
+		final String host = "localhost"; // 127.0.0.1 or ip address 
+		final String port = "9092"; 
+		
+		// set all properties needed 
+		Properties prop = new Properties(); 
+		// or your key can be -> "bootstrap.servers"
+		prop.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, host +":" + port); 
+		prop.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName()); 
+		prop.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName()); 
+		
+		// creating a producer 
+		KafkaProducer<String, String> producer = new KafkaProducer<>(prop);
+		
+		// creating a producer record 
+		ProducerRecord<String, String> producerRecord = 
+				new ProducerRecord<String, String>("topic-with-partition1", "Message From Java"); 
+		
+		// you ar attaching the producer record to producer 
+		producer.send(producerRecord);
+		
+//		tell the producer to send all data and block until done - synchronous 
+		producer.flush(); 
+		
+		// close the connection 
+		producer.close(); 
+	
+		logger.info("Message Send Successfully");
+	}
+}
+
+
+
+
+
+
+
