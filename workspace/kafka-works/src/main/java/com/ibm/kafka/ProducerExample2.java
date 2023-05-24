56706f4a3bf0323fb096b97c519347aad76b8706
Naveen <adith.naveen@gmail.com>
2023-05-24 16:24:34 +0530

    updated producer with callback

--- workspace/kafka-works/src/main/java/com/ibm/kafka/ProducerExample2.java
+++ workspace/kafka-works/src/main/java/com/ibm/kafka/ProducerExample2.java
@@ -1,17 +1,20 @@
 package com.ibm.kafka;
 
+import java.util.Date;
 import java.util.Properties;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ProducerExample01 {
+public class ProducerExample2 {
 	
-	private static final Logger logger = LoggerFactory.getLogger(ProducerExample01.class.getName());
+	private static final Logger logger = LoggerFactory.getLogger(ProducerExample2.class.getName());
 	
 	public static void main(String[] args) {
 		logger.info("Hi I'm from slf4j");
@@ -35,15 +38,33 @@ public class ProducerExample01 {
 				new ProducerRecord<String, String>("topic-with-partition1", "Message From Java"); 
 		
 		// you are attaching the producer record to producer 
-		producer.send(producerRecord);
+		producer.send(producerRecord, new Callback() {
+			
+			@Override
+			public void onCompletion(RecordMetadata metadata, Exception exception) {
+				if(exception == null) {
+					
+					logger.info("Message Sent" );
+					
+					logger.info(
+							" Topic "+ metadata.topic() +
+							" Partition " + metadata.partition() + 
+							" Time Stamp "+ new Date(metadata.timestamp())
+						); 
+					
+					
+				}else {
+					logger.error("Message delevery failed, {}", exception);
+				}
+			}
+		});
 		
 //		tell the producer to send all data and block until done - synchronous 
 		producer.flush(); 
 		
 		// close the connection 
 		producer.close(); 
-	
-		logger.info("Message Send Successfully");
+	 
 	}
 }
 
